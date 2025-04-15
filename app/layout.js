620f0cf225a9cfd382e490a5af966f4b96a31d20
Régis Daum <rdaum@john-steel.com>
2025-04-15 11:37:06 +0200

    Remove GA because 502

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@ import { ViewTransitions } from "next-view-transitions";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import ScrollToTopButton from "@/components/ui/ScrollToTopButton";
-import { Analytics } from "./analytics";
 
 const outfit = Outfit({
 	variable: "--font-outfit",
@@ -43,24 +42,6 @@ export default function RootLayout({ children }) {
 		<ViewTransitions>
 			<html lang="en">
 				<head>
-					{/* Script Google Analytics */}
-					<script
-						async
-						src="https://www.googletagmanager.com/gtag/js?id=G-345118589"
-					></script>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-345118589', {
-                page_path: window.location.pathname,
-              });
-            `,
-						}}
-					/>
-
 					<link rel="manifest" href="/manifest.json" />
 					<link rel="icon" href="/icons/icon-192x192.png" />
 					<meta name="theme-color" content="#ffffff" />
@@ -69,7 +50,6 @@ export default function RootLayout({ children }) {
 				<body
 					className={`${outfit.variable} ${urbanist.variable} antialiased`}
 				>
-					<Analytics />
 					<Header />
 					<main className="relative z-10 bg-white">{children}</main>
 					<ScrollToTopButton />
