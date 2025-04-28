303b600795c2eda50aec17b359db1cdaacdb23ff
Régis Daum <rdaum@john-steel.com>
2025-04-28 09:55:53 +0200

    Font size markee logos

--- components/offres/VelocityText.jsx
+++ components/offres/VelocityText.jsx
@@ -8,6 +8,8 @@ import {
 } from "framer-motion";
 import React, { useRef } from "react";
 import { FiArrowDown } from "react-icons/fi";
+import VanishParagraphs from "../ui/VanishParagraphs";
+import Button from "@/components/ui/Button";
 
 export const VelocityText = () => {
 	const targetRef = useRef(null);
@@ -22,71 +24,42 @@ export const VelocityText = () => {
 	const skewXRaw = useTransform(scrollVelocity, [-1, 1], ["45deg", "-45deg"]);
 	const skewX = useSpring(skewXRaw, { mass: 3, stiffness: 400, damping: 50 });
 
-	const xRaw = useTransform(scrollYProgress, [0, 1], [0, -2000]);
+	const xRaw = useTransform(scrollYProgress, [0, 1], [0, -1200]);
 	const x = useSpring(xRaw, { mass: 3, stiffness: 400, damping: 50 });
 
 	return (
-		<section
-			ref={targetRef}
-			className="h-[500vh] bg-neutral-50 text-neutral-950"
-		>
+		<section ref={targetRef} className="h-[500vh] py-16">
 			<div className="sticky top-0 flex h-screen flex-col justify-between overflow-hidden">
-				<CenterCopy />
+				<div className="wrapper-small py-24">
+					<VanishParagraphs>
+						<h2 className="title-text text-2xl sm:text-3xl md:sm:text-4xl font-semibold">
+							Chaque site web est unique,
+						</h2>
+						<h3 className="title-text text-xl sm:text-2xl md:sm:text-3xl text-neutral-500 font-semibold mb-8">
+							car chaque entreprise l'est également.
+						</h3>
+						<p>
+							Soyez exigeant : nous créons ainsi votre site «
+							sur-mesure », afin qu'il vous habille comme un
+							vêtement taillé pour vous ! Ni trop grand, ni trop
+							petit, dans une matière seyante et avec un budget
+							qui vous correspond. Jetez un oeil à quelques unes
+							de nos créations pour vous aider à définir vos
+							besoins.
+						</p>
+					</VanishParagraphs>
+					<Button icon={null} href="/projets" className="mt-8">
+						Découvrez nos projets
+					</Button>
+				</div>
 				<motion.p
 					style={{ skewX, x }}
-					className="origin-bottom-left whitespace-nowrap text-8xl title-font font-black! uppercase leading-[0.85] md:text-10xl md:leading-[0.85]"
+					className="origin-bottom-left pb-26 pl-12 whitespace-nowrap font-black! uppercase leading-[0.85] text-7xl md:leading-[0.85]"
 				>
 					Design propre, performance assurée.
 				</motion.p>
-				<ScrollArrow />
+				{/* <ScrollArrow /> */}
 			</div>
 		</section>
-	);
-};
-
-const CenterCopy = () => {
-	return (
-		<div className="flex items-center justify-center px-4">
-			<img
-				src="https://images.unsplash.com/photo-1622599511051-16f55a1234d0?q=80&w=2536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-				alt="Placeholder image"
-				className="mr-2 h-full w-20 bg-neutral-200 object-cover"
-			/>
-			<h1 className="text-3xl font-bold text-neutral-400 sm:text-5xl md:text-7xl">
-				Life is short. <br />
-				Don't waste it. <br />
-				It's time to{" "}
-				<span className="inline-block -skew-x-[18deg] font-black text-neutral-950">
-					SHIFT.
-				</span>
-			</h1>
-		</div>
-	);
-};
-
-const ScrollArrow = () => {
-	return (
-		<>
-			<div className="absolute left-4 top-1/2 hidden -translate-y-1/2 text-xs lg:block">
-				<span
-					style={{
-						writingMode: "vertical-lr",
-					}}
-				>
-					SCROLL
-				</span>
-				<FiArrowDown className="mx-auto" />
-			</div>
-			<div className="absolute right-4 top-1/2 hidden -translate-y-1/2 text-xs lg:block">
-				<span
-					style={{
-						writingMode: "vertical-lr",
-					}}
-				>
-					SCROLL
-				</span>
-				<FiArrowDown className="mx-auto" />
-			</div>
-		</>
 	);
 };
