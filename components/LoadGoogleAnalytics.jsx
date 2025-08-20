d7949d71732a511b6210474afc7ee766b59d6240
Régis Daum <rdaum@john-steel.com>
2025-08-20 14:33:48 +0200

    Changed Google Analytics ID

--- components/LoadGoogleAnalytics.jsx
+++ components/LoadGoogleAnalytics.jsx
@@ -3,32 +3,32 @@
 import { useEffect } from "react";
 
 export default function LoadGoogleAnalytics() {
-	useEffect(() => {
-		const loadScript = () => {
-			// Injecte le script principal
-			const script = document.createElement("script");
-			script.src =
-				"https://www.googletagmanager.com/gtag/js?id=G-345118589";
-			script.async = true;
-			document.head.appendChild(script);
+   useEffect(() => {
+      const loadScript = () => {
+         // Injecte le script principal
+         const script = document.createElement("script");
+         script.src =
+            "https://www.googletagmanager.com/gtag/js?id=G-8KZZ89ZK05";
+         script.async = true;
+         document.head.appendChild(script);
 
-			// Configure GA après chargement
-			window.dataLayer = window.dataLayer || [];
-			function gtag() {
-				window.dataLayer.push(arguments);
-			}
-			gtag("js", new Date());
-			gtag("config", "G-345118589");
-		};
+         // Configure GA après chargement
+         window.dataLayer = window.dataLayer || [];
+         function gtag() {
+            window.dataLayer.push(arguments);
+         }
+         gtag("js", new Date());
+         gtag("config", "G-345118589");
+      };
 
-		// Attendre que la page soit complètement chargée
-		if (document.readyState === "complete") {
-			loadScript();
-		} else {
-			window.addEventListener("load", loadScript);
-			return () => window.removeEventListener("load", loadScript);
-		}
-	}, []);
+      // Attendre que la page soit complètement chargée
+      if (document.readyState === "complete") {
+         loadScript();
+      } else {
+         window.addEventListener("load", loadScript);
+         return () => window.removeEventListener("load", loadScript);
+      }
+   }, []);
 
-	return null;
+   return null;
 }
