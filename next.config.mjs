ae6bf9e22de4f0f23c3701fbfad656b7c524925c
Régis Daum <rdaum@john-steel.com>
2025-08-20 16:13:16 +0200

    Redirections realisations vers projets

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,27 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Configure `pageExtensions` to include markdown and MDX files
-	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-	// Optionally, add any other Next.js config below
+   // Configure `pageExtensions` to include markdown and MDX files
+   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
+   async redirects() {
+      return [
+         {
+            source: "/realisations/:path*", // toutes les pages sous /realisations/...
+            destination: "/projets",
+            permanent: true, // 301
+         },
+         {
+            source: "/realisations", // la page /realisations elle-même
+            destination: "/projets",
+            permanent: true,
+         },
+      ];
+   },
 };
 
 const withMDX = createMDX({
-	// Add markdown plugins here, as desired
+   // Add markdown plugins here, as desired
 });
 
 // Merge MDX config with Next.js config
